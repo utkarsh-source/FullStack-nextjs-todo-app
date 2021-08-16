66842ec0dac7ef698b5f8a5d8c6b31aa1e6d1305
Utkarsh_agrawal <jiren0359san@gmail.com>
2021-08-16 23:11:54 +0530

    Improved UI

--- components/Todo.js
+++ components/Todo.js
@@ -10,7 +10,7 @@ import TodoInput from "./TodoInput"
 
 
 export const Loader = ({className}) => {
-    return <L className={className} type='spin' color="green" width={25} height={25}/>
+    return <L className={className} type='spin' color="blue" width={18} height={18}/>
 }
 
 
@@ -50,17 +50,13 @@ function Todo() {
 
     const toggle = {
         hidden: {
-            scale: 0.8,
+            scale: 0.9,
             opacity: 0,
         },
         vissible: {
             scale: 1,
             opacity: 1,
         },
-        transition: {
-                    type: 'tween',
-                    duration: 0.25
-                }
     }
 
 
@@ -69,18 +65,21 @@ function Todo() {
     return (
         <section className="bg-yellow-100 bg-opacity-30 md:w-full w-screen md:py-5 min-h-screen relative flex md:items-center justify-center ">
             <AnimateSharedLayout >
-            <motion.div layout transition={{type:"tween" , duration: 0.2}} className="relative md:w-1/2 w-full h-auto border-2 border-gray-100 rounded-lg shadow-lg px-2 py-4">
-                    <motion.h1 layout className="relative  flex items-center justify-center text-center font-semibold text-xl mb-8 py-3">Real Time Todo <button onClick={openInputBox} className="focus:outline-none absolute h-8 w-8  right-0 top-1/2 transform -translate-y-1/2 rounded-3xl bg-blue-500 grid cursor-default place-content-center">
-                        <FaPlus className={`${isOpen && "rotate-45 duration-300"} active:scale-95 transform text-white font-semibold text-base transition-transform`} /></button>
-                        <FaSignOutAlt onClick={()=>logout(dispatch)} aria-label="logout button" className="hover:scale-110 absolute top-1/2 transform -translate-y-1/2 left-0 text-3xl cursor-pointer text-red-500"/>
+            <motion.div layout className="relative md:w-1/2 w-full h-auto border-2 border-gray-100 rounded-lg shadow-lg px-2 py-4">
+                    <motion.h1 layout className="relative flex items-center justify-between text-center font-semibold text-3xl mb-8 pb-3"><span>T<span className="text-blue-600">o</span>D<span className="text-blue-600">oo</span></span>
+                        <div className="flex items-center space-x-4 px-4 py-2 border-2 shadow-sm border-gray-300 rounded-full">
+                            <FaSignOutAlt onClick={()=>logout(dispatch)} aria-label="logout button" className="hover:scale-110 text-3xl cursor-pointer text-red-500"/>
+                            <button onClick={openInputBox} className="focus:outline-none h-8 w-8 rounded-full bg-blue-500 grid cursor-default place-content-center">
+                            <FaPlus className={`${isOpen && "rotate-45 duration-300"} active:scale-95 transform text-white font-semibold text-base transition-transform`} /></button>
+                        </div>
                     </motion.h1>
                 <AnimatePresence>
                         {isOpen &&
                             <motion.div layout><TodoInput toggle={toggle}/></motion.div> 
                         }
                     </AnimatePresence>
-                    <motion.p layout transition={{type:"tween" , duration: 0.2}} className="flex px-2 space-x-4 justify-end w-full mt-8">{isDeletingAllTodo ? <Loader /> :todosDetails.length >= 1 &&  <motion.button transition={{type:"tween" , duration: 0.2}} layout onClick={deleteAllTodos} className={`inline-block text-white bg-blue-500 px-4 py-1 rounded-full text-sm font-semibold `}>Delete all : {todosDetails.length}</motion.button>} {isDeletingCompletedTodo ? <Loader /> : todosDetails.completedTodos >= 1 && <button onClick={deleteCompletedTodos} className={`${todosDetails.completedTodos <1 && "hidden"} text-white bg-green-500 px-4 py-1 rounded-full text-sm font-semibold inline-block`}>Delete completed : {todosDetails.completedTodos}</button>}</motion.p>
-                    <motion.div layout transition={{type:"tween" , duration: 0.2}}  className="relative w-auto grid grid-cols-1 gap-y-3 py-4">
+                    <motion.p layout className="flex px-2 space-x-4 justify-end w-full mt-8">{isDeletingAllTodo ? <Loader /> :todosDetails.length >= 1 &&  <motion.button layout onClick={deleteAllTodos} className={`inline-block text-white bg-blue-500 px-4 py-1 rounded-full text-sm font-semibold `}>Delete all : {todosDetails.length}</motion.button>} {isDeletingCompletedTodo ? <Loader /> : todosDetails.completedTodos >= 1 && <button onClick={deleteCompletedTodos} className={`${todosDetails.completedTodos <1 && "hidden"} text-white bg-green-500 px-4 py-1 rounded-full text-sm font-semibold inline-block`}>Delete completed : {todosDetails.completedTodos}</button>}</motion.p>
+                    <motion.div layout className="relative w-auto grid grid-cols-1 gap-y-3 py-4">
                         {todosLoading ? <Loader className="mx-auto text-2xl"/> : !todos?.length ? <p className="text-center text-blue-500 text-lg font-semibold border-2 border-gray-300  rounded-md py-5">Ops : No Tasks yet!</p> : todos.map((todo, index) => {
                             return (
                                 <Fragment key={todo.id}>
--- components/TodoItem.js
+++ components/TodoItem.js
@@ -45,41 +45,29 @@ function TodoItem({ todosLoading, todo, variants, Loader, todoNum}) {
         })
     }
 
-    
-    const line_through = {
-        hide: {
-            scaleX: 0,
-            originX: 0,
-        },
-        show: {
-            scaleX: 1
-        }
-    }
-
     return (
         <>
             <AnimatePresence>
-                        <motion.div key="title" transition={{type:"tween" , duration: 0.2}} layout className={`relative flex ${isExpanded && "ring-2 border-blue-500"} items-center py-5 shadow-sm w-full border-2 border-gray-300 rounded-2xl px-4`}>
+                        <motion.div key="title" layout className={`relative flex ${isExpanded && "ring-2 border-blue-500"} items-center py-5 shadow-sm w-full border-2 border-gray-300 rounded-2xl px-4`}>
                             {isUpdating ? <Loader/> : <FaCheckCircle onClick={handleCompleteTodo} className={`${!todosLoading &&todo.is_completed && "text-blue-500"} ring-2 ring-blue-500 cursor-pointer hover:scale-105 active:scale-95 transform rounded-full text-lg font-semibold text-transparent `} />}
                             <p className={`ml-3 relative font-semibold px-2 text-base`}>{todo.title}
-                                <motion.span transition={{type:"tween" , duration: 0.2}} variants={line_through} animate={`${(!todosLoading && todo.is_completed) ? 'show' : 'hide'}`}  className="block absolute w-full left-0 top-1/2 rounded-full -translate-y-1/2 h-0.5 bg-blue-500" ></motion.span>
                             </p>
                             <span className="ml-auto inline-flex space-x-4 items-center">
-                                <FaChevronDown onClick={handleExpand} className={`${isExpanded && "rotate-180 duration-200"} 
-                        text-xl transform transition-transform select-none hover:scale-125 font-semibold text-blue-500 `} />
-                                <FaPenAlt onClick={() => handleUpdateExpand(true)} className="text-indigo-500 text-lg cursor-pointer transform hover:scale-125" />
-                                {isDeleting ? <Loader /> : <FaTrashAlt onClick={handleDeleteTodo} className="text-red-600 text-lg cursor-pointer transform hover:scale-125" />}
+                                <FaChevronDown onClick={handleExpand} className={`${isExpanded && "rotate-180 duration-200 transition-transform"} 
+                        text-xl transform  select-none hover:scale-125 font-semibold text-blue-500 `} />
+                                <FaPenAlt onClick={() => handleUpdateExpand(true)} className="text-indigo-500 text-lg transform hover:scale-125" />
+                                {isDeleting ? <Loader /> : <FaTrashAlt onClick={handleDeleteTodo} className="text-red-600 text-lg transform hover:scale-125" />}
                             </span>
                         </motion.div>
                     {isExpanded &&
-                    <motion.p layout transition={{ type: "tween", duration: 0.2 }} key="description" variants={variants} animate="vissible" initial="hidden" exit="hidden" className="border-2 border-gray-300 font-semibold text-gray-500 text-sm rounded-2xl px-4 py-5 ">
+                    <motion.p layout key="description" variants={variants} animate="vissible" initial="hidden" exit="hidden" className="border-2 border-gray-300 font-semibold text-gray-500 text-sm rounded-2xl px-4 py-5 ">
                     <span className="block text-blue-500 text-sm font-semibold pb-2">Date</span>
                     {new Date(todo.created_at).toLocaleDateString("en-US", { dateStyle: "long" })}
                             <span className="block text-blue-500 text-sm font-semibold mt-4 py-2">Description</span>
                             {todo.description}
                         </motion.p>
                     }
-                    {toggledUpdateBox && <motion.div transition={{type:"tween" , duration: 0.2}} key="update todo box" layout className="mt-3"> <TodoInput todo={todo} isCompleted={isCompleted} todoNum={todoNum} handleUpdateExpand={handleUpdateExpand} toggle={variants} toUpdate/></motion.div>}
+                    {toggledUpdateBox && <motion.div key="update todo box" layout className="mt-3"> <TodoInput todo={todo} isCompleted={isCompleted} todoNum={todoNum} handleUpdateExpand={handleUpdateExpand} toggle={variants} toUpdate/></motion.div>}
             </AnimatePresence>
         </>
     )
